64179bc0d1d22847a4c93d797e708638533aa7eb
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-12-04 20:38:32 +0900

    added numbers components.

---
+++ src/components/Num.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Digit } from '../types/numbers';
+	export let number: Digit;
+</script>
+
+<span class="num" class:incorrect={!number.isInputSameAsAnswer()}>
+	{number.input}
+</span>
+
+<style>
+	.num {
+		font-family: 'DSEG7-Classic';
+		font-weight: bold;
+	}
+</style>
---
+++ src/components/Numbers.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { FiveDigits } from '../types/numbers';
+	import Num from './Num.svelte';
+	export let numbers: FiveDigits;
+</script>
+
+<div class="numbers">
+	{#each numbers.toArray() as number}
+		{#if typeof number.input !== 'undefined'}
+			<span>
+				<Num {number} />
+			</span>
+		{/if}
+	{/each}
+</div>
+
+<style>
+	.numbers {
+		display: flex;
+		justify-content: flex-end;
+	}
+</style>
