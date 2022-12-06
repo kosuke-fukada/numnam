2cd12ef4be5266d35982f9ce940a5d8399709bd2
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-12-07 00:25:52 +0900

    added header component.

---
+++ src/components/Header.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	const newGameDispatcher = () => {
+		dispatch('newGame');
+	};
+</script>
+
+<div>
+	<button class="new-game-button" on:click={newGameDispatcher}>NEW GAME</button>
+</div>
+
+<style lang="scss">
+	.new-game-button {
+		width: min(50%, 150px);
+		height: min(5vh, 50px);
+		position: absolute;
+		margin: 0 auto;
+		top: 30px;
+		right: 100px;
+		border-radius: 20px;
+		border: none;
+		font-size: min(2.5vh, 20px);
+		font-weight: bold;
+		color: #ffffff;
+		background-color: #ec5990;
+		@media screen and (max-width: 700px) {
+			top: 15px;
+			right: 10px;
+		}
+	}
+</style>
