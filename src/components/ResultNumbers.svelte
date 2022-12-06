4adf63e99099a32eff1e06c654b02b81c78b07ed
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-12-06 22:01:29 +0900

    added result numbers component.

---
+++ src/components/ResultNumbers.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import type { ResultDigits } from 'src/types/numbers';
+	import Numbers from './Numbers.svelte';
+
+	export let results: ResultDigits[] = [];
+</script>
+
+<div>
+	{#each results.reverse() as result, index}
+		<span class="title">{index === 1 ? '1st' : '2nd'} Trial</span>
+		<Numbers numbers={result} result={result.result} />
+	{/each}
+</div>
+
+<style>
+	.title {
+		color: #ffffff;
+		font-size: 25px;
+		font-weight: bold;
+		font-style: italic;
+		margin-left: 15px;
+	}
+</style>
