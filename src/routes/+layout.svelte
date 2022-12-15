414720393f1c32e3135c1cde214b6abe4e585c8d
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-12-16 00:10:22 +0900

    added meta tags.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script>
+	import { PUBLIC_APP_URL } from '$env/static/public';
 	import 'dseg/css/dseg.css';
 </script>
+
+<svelte:head>
+	<title>Numnam | Guess what the number is.</title>
+	<meta name="description" content="Numnam is a five-digit number guessing game." />
+	<meta property="og:title" content="Numnam | Guess what the number is." />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={PUBLIC_APP_URL} />
+	<meta property="og:description" content="Numnam is a five-digit number guessing game." />
+	<meta property="og:image" content="{PUBLIC_APP_URL}/images/ogp.png" />
+	<meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
 
 <div class="container">
 	<slot />
